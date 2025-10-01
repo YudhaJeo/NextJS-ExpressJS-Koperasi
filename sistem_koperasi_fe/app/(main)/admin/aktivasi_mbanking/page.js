cf9c7e86108d487bb00db9b9fd501ba59cf2964a
BagasYudha <bayud.kematian@gmail.com>
2025-10-01 16:00:24 +0700

    ADD: Aktivasi Mbanking - Fitur filter tanggal

--- sistem_koperasi_fe/app/(main)/admin/aktivasi_mbanking/page.js
+++ sistem_koperasi_fe/app/(main)/admin/aktivasi_mbanking/page.js
@@ -6,6 +6,7 @@ import HeaderBar from '@/app/components/headerbar';
 import TabelData from './components/tabelData';
 import ToastNotifier from '@/app/components/toastNotifier';
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+import FilterTanggal from '@/app/components/filterTanggal';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -13,6 +14,9 @@ const Page = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const toastRef = useRef(null);
+  const [originalData, setOriginalData] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -23,6 +27,7 @@ const Page = () => {
     try {
       const res = await axios.get(`${API_URL}/aktivasi_mbanking`);
       setData(res.data.data);
+      setOriginalData(res.data.data)
     } catch (err) {
       console.error('Gagal ambil data:', err);
       toastRef.current?.showToast('01', 'Gagal mengambil data');
@@ -84,6 +89,25 @@ const Page = () => {
     });
   };
 
+  const handleDateFilter = () => {
+    if (!startDate && !endDate) return setData(originalData);
+
+    const filtered = originalData.filter((item) => {
+      const visitDate = new Date(item.datetime);
+      const from = startDate ? new Date(startDate.setHours(0, 0, 0, 0)) : null;
+      const to = endDate ? new Date(endDate.setHours(23, 59, 59, 999)) : null;
+      return (!from || visitDate >= from) && (!to || visitDate <= to);
+    });
+
+    setData(filtered);
+  };
+
+  const resetFilter = () => {
+    setStartDate(null);
+    setEndDate(null);
+    setData(originalData);
+  };
+
   return (
     <div className="card">
       <ToastNotifier ref={toastRef} />
@@ -91,18 +115,28 @@ const Page = () => {
 
       <h3 className="text-xl font-semibold mb-3">Aktivasi Mbanking</h3>
 
-      <HeaderBar
-        title=""
-        placeholder="Cari nama atau kode"
-        onSearch={(keyword) => {
-          if (!keyword) return fetchData();
-          const filtered = data.filter((item) =>
-            item.kode_perusahaan.toLowerCase().includes(keyword.toLowerCase()) ||
-            item.fullname.toLowerCase().includes(keyword.toLowerCase())
-          );
-          setData(filtered);
-        }}
-      />
+      <div className="flex items-center justify-content-between">
+        <FilterTanggal
+          startDate={startDate}
+          endDate={endDate}
+          setStartDate={setStartDate}
+          setEndDate={setEndDate}
+          handleDateFilter={handleDateFilter}
+          resetFilter={resetFilter}
+        />
+        <HeaderBar
+          title=""
+          placeholder="Cari nama atau kode"
+          onSearch={(keyword) => {
+            if (!keyword) return fetchData();
+            const filtered = data.filter((item) =>
+              item.kode_perusahaan.toLowerCase().includes(keyword.toLowerCase()) ||
+              item.fullname.toLowerCase().includes(keyword.toLowerCase())
+            );
+            setData(filtered);
+          }}
+        />
+      </div>
 
       <TabelData
         data={data}
