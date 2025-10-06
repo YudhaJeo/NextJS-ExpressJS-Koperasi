ed187806e0858475ae307f1583456dc1a4a7a350
AlyaZahra <alyazahs13@gmail.com>
2025-10-06 14:14:14 +0700

    ADD: auth - pembagian akses role

--- sistem_koperasi_fe/layout/AppMenu.js
+++ sistem_koperasi_fe/layout/AppMenu.js
@@ -1,32 +1,80 @@
+'use client';
 import React, { useContext } from "react";
 import AppMenuitem from "./AppMenuitem";
+import { LayoutContext } from "./context/layoutcontext";
 import { MenuProvider } from "./context/menucontext";
+import Cookies from "js-cookie";
 
 const AppMenu = () => {
+  const { layoutConfig } = useContext(LayoutContext);
+  const role = Cookies.get("role");
 
-  const model = [
-    {
-      items: [
-        { label: "Dashboard Utama", icon: "pi pi-fw pi-chart-bar", to: "/" },
-        { label: "Aktivasi Mpay", icon: "pi pi-fw pi-wallet", to: "/admin/aktivasi_mpay" },
-        {
-          label: "Laporan Mpay",
-          icon: "pi pi-fw pi-list",
-          items: [{ label: "Simpanan", icon: "pi pi-fw pi-money-bill", to: "/admin/laporan_mpay/simpanan" }],
-        },
-        { label: "Aktivasi Mbanking", icon: "pi pi-fw pi-mobile", to: "/admin/aktivasi_mbanking" },
-        { label: "Config Biaya Admin", icon: "pi pi-fw pi-credit-card", to: "/admin/config_biaya_admin" },
-        {
-          label: "Laporan Mbanking",
-          icon: "pi pi-fw pi-list",
-          items: [{ label: "History Transaksi", icon: "pi pi-fw pi-history", to: "/admin/laporan_mbanking/history_transaksi" }],
-        },
-        { label: "Perusahaan", icon: "pi pi-fw pi-building-columns", to: "/admin/perusahaan" },
-        { label: "Users", icon: "pi pi-fw pi-user", to: "/admin/users" },
-        { label: "Roles", icon: "pi pi-fw pi-users", to: "/admin/roles" },
-      ],
-    },
-  ];
+  let model = [];
+
+  if (role === "admin") {
+    model = [
+      {
+        items: [
+          { label: "Dashboard Utama", icon: "pi pi-fw pi-chart-bar", to: "/" },
+          { label: "Aktivasi Mpay", icon: "pi pi-fw pi-wallet", to: "/admin/aktivasi_mpay" },
+          {
+            label: "Laporan Mpay",
+            icon: "pi pi-fw pi-list",
+            items: [{ label: "Simpanan", icon: "pi pi-fw pi-money-bill", to: "/admin/laporan_mpay/simpanan" }],
+          },
+          { label: "Aktivasi Mbanking", icon: "pi pi-fw pi-mobile", to: "/admin/aktivasi_mbanking" },
+          { label: "Config Biaya Admin", icon: "pi pi-fw pi-credit-card", to: "/admin/config_biaya_admin" },
+          {
+            label: "Laporan Mbanking",
+            icon: "pi pi-fw pi-list",
+            items: [{ label: "History Transaksi", icon: "pi pi-fw pi-history", to: "/admin/laporan_mbanking/history_transaksi" }],
+          },
+          { label: "Perusahaan", icon: "pi pi-fw pi-building-columns", to: "/admin/perusahaan" },
+          { label: "Users", icon: "pi pi-fw pi-user", to: "/admin/users" },
+          { label: "Roles", icon: "pi pi-fw pi-users", to: "/admin/roles" },
+        ],
+      },
+    ];
+  } else if (role === "vendor") {
+    model = [
+      {
+        items: [
+          { label: "Dashboard Utama", icon: "pi pi-fw pi-chart-bar", to: "/" },
+          { label: "Aktivasi Mpay", icon: "pi pi-fw pi-wallet", to: "/admin/aktivasi_mpay" },
+          {
+            label: "Laporan Mpay",
+            icon: "pi pi-fw pi-list",
+            items: [{ label: "Simpanan", icon: "pi pi-fw pi-money-bill", to: "/admin/laporan_mpay/simpanan" }],
+          },
+          { label: "Aktivasi Mbanking", icon: "pi pi-fw pi-mobile", to: "/admin/aktivasi_mbanking" },
+          { label: "Config Biaya Admin", icon: "pi pi-fw pi-credit-card", to: "/admin/config_biaya_admin" },
+          {
+            label: "Laporan Mbanking",
+            icon: "pi pi-fw pi-list",
+            items: [{ label: "History Transaksi", icon: "pi pi-fw pi-history", to: "/admin/laporan_mbanking/history_transaksi" }],
+          },
+        ],
+      },
+    ];
+  } else if (role === "user") {
+    model = [
+      {
+        items: [
+          { label: "Dashboard Utama", icon: "pi pi-fw pi-chart-bar", to: "/" },
+          {
+            label: "Laporan Mpay",
+            icon: "pi pi-fw pi-list",
+            items: [{ label: "Simpanan", icon: "pi pi-fw pi-money-bill", to: "/admin/laporan_mpay/simpanan" }],
+          },
+          {
+            label: "Laporan Mbanking",
+            icon: "pi pi-fw pi-list",
+            items: [{ label: "History Transaksi", icon: "pi pi-fw pi-history", to: "/admin/laporan_mbanking/history_transaksi" }],
+          },
+        ],
+      },
+    ];
+  }
 
   return (
     <MenuProvider>
